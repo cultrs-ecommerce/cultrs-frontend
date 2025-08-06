80df5fb829d40f2e4f981f107f60a4e976e329e3
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2025-08-05 23:49:09 -0700

    added env inside of vite.config.ts for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,15 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: process.env.VITE_PORT ? parseInt(process.env.VITE_PORT, 10) : 3000,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: process.env.VITE_BASE_PATH || "/cultrs-frontend",
 }));
