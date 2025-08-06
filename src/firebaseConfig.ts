920cb678313bcd153585cee81dddc6ffd9784282
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2025-08-05 23:36:54 -0700

    added env support

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,14 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const env = import.meta.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDwGmNHxvSEqKcPFkz3NeiIg4FzCPpsu5w",
-  authDomain: "cultrs-backend.firebaseapp.com",
-  projectId: "cultrs-backend",
-  storageBucket: "cultrs-backend.firebasestorage.app",
-  messagingSenderId: "877519543512",
-  appId: "1:877519543512:web:367eaecfdf70f27013cbad",
-  measurementId: "G-8PTF75J3KD"
+  apiKey: env.VITE_FIREBASE_API_KEY,
+  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.VITE_FIREBASE_APP_ID,
+  measurementId: env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
