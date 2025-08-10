7db1e405dc3b2d401268d0ff8bd2fa8d669d7e3b
lakshmakhija0904 <lakshmakhija0904@gmail.com>
2025-08-10 03:55:19 +0000

    added screen width changes

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -99,7 +99,7 @@ const Shop = () => {
               <div
                 className={
                   viewMode === "grid"
-                    ? "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6"
+                    ? "grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 gap-6"
                     : "space-y-4"
                 }
               >
